442b0edd2927f4d0391f528ac0b6e5a8feb87360
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-11-18 23:56:13 +0500

    voucher page responsive

--- Codefyze/Task-01/src/pages/voucher.js
+++ Codefyze/Task-01/src/pages/voucher.js
@@ -24,22 +24,22 @@ const geistMono = localFont({
 export default function Voucher() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-w-screen font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} flex flex-col w-screen font-[family-name:var(--font-geist-sans)]`}
     >
-      <div className="flex justify-between navbar bg-[#1F2A37] h-28">
-        <div className="road"></div>
-        <div className="flex flex-col items-center w-[60vw] p-10 justify-center">
-          <div className="text-white text-3xl mb-2 font-semibold">Get your gift voucher now!</div>
-          <div className="flex items-center gap-8">
-            <Button className="bg-[#F0B000] px-3 text-center rounded-2xl text-sm py-1 border border-[#F0B000] h-7" variant="primary">Step 1 - Gift Voucher</Button>
-            <Button className="bg-[#FFFFFF] px-3 text-center rounded-2xl text-sm py-1 border border-[#F0B000] h-7" variant="primary">Step 2 - Checkout</Button>
+      <div className="flex justify-between navbar bg-[#1F2A37] w-full">
+        <div className="w-full h-20 bg-road bg-center bg-cover sm:w-[20vw] sm:h-auto"></div>
+        <div className="hidden flex-col sm:flex items-center w-[60vw] p-7 justify-center">
+          <div className="text-white text-2xl md:text-3xl mb-2 font-semibold">Get your gift voucher now!</div>
+          <div className="flex flex-col gap-2 items-center sm:flex-row sm:gap-8">
+            <Button className="w-full sm:w-auto bg-[#F0B000] px-3 text-center rounded-2xl text-sm py-1 border border-[#F0B000] h-7" variant="primary">Step 1 - Gift Voucher</Button>
+            <Button className="w-full sm:w-auto bg-[#FFFFFF] px-3 text-center rounded-2xl text-sm py-1 border border-[#F0B000] h-7" variant="primary">Step 2 - Checkout</Button>
           </div>
         </div>
-        <div className="road"></div>
+        <div className="hidden sm:block bg-road bg-center bg-cover sm:w-[20vw]"></div>
       </div>
-      <div className="header flex mx-32 mt-11">
-        <div className="w-[50%] pt-14">
-          <div className="mb-3 font-semibold"><Label className="text-base">Delivery Method</Label></div>
+      <div className="mx-4 header flex flex-col-reverse md:mx-10 md:flex-row xl:mx-32 mt-11">
+        <div className="w-full pt-14 md:w-1/3 lg:w-1/2">
+          <div className="mb-3 font-semibold"><Label className="text-xl md:text-base">Delivery Method</Label></div>
           <RadioGroup defaultValue="option-one">
             <div className="flex items-center space-x-2">
               <RadioGroupItem value="email" id="email" className="custom-radio" />
@@ -68,42 +68,43 @@ export default function Voucher() {
               </div>
             </div>
           </RadioGroup>
-          <div className="w-max bg-[#1F2A37] mt-28 px-3 py-2 rounded-lg text-white font-semibold">
-            <span className="text-lg font-medium">How many hours of lessons would you like purchase?</span>
-            <span className="flex items-center justify-between mt-2">
-              <div className="flex items-center gap-4">
+          <div className="w-full md:w-max bg-[#1F2A37] mt-8 md:max-lg:mt-52 px-3 py-2 rounded-lg text-white font-semibold">
+            <span className="text-sm md:text-lg font-medium">How many hours of lessons would you like purchase?</span>
+            <span className="flex flex-col gap-y-5 pt-5 items-center justify-between mt-2 sm:flex-row sm:pt-0">
+              <div className="w-full flex justify-around items-center gap-4">
                 <Combobox />
                 <Label className="text-[#F0B000] font-semibold">Lesson Hour/s</Label>
               </div>
-              <div>
+              <div className="w-full flex justify-around items-center">
                 <Label className="text-white font-semibold">TOTAL &nbsp;&nbsp;&nbsp;&nbsp;$532</Label>
                 <Label className="text-white bg-[#F83B00] ml-3 py-1 px-5 rounded-full text-[0.7rem] font-normal">5% OFF</Label>
               </div>
             </span>
-            <div className="text-[0.5rem] ml-4 mt-1 text-muted-foreground">
+            <div className="text-[0.5rem] ml-4  mt-1 text-muted-foreground">
               Click to edit
             </div>
           </div>
           <div className="form flex flex-col mt-8">
             <Label className="text-base font-semibold">To</Label>
-            <Input type="name" placeholder="Recipient first name" />
-            <Input type="email" placeholder="Recipient email" />
+            <Input type="name" placeholder="Recipient first name"  className="bg-gray-300"/>
+            <Input type="email" placeholder="Recipient email"  className="bg-gray-300"/>
             <Label className="text-base font-semibold mt-4">From</Label>
-            <Input type="name" placeholder="Sender name" />
-            <Input type="email" placeholder="Sender email" />
+            <Input type="name" placeholder="Sender name"  className="bg-gray-300"/>
+            <Input type="email" placeholder="Sender email"  className="bg-gray-300"/>
             <Label className="text-base font-semibold mt-4">Message</Label>
-            <Textarea placeholder="Type your message here." className="min-h-28"/>
+            <Textarea placeholder="Type your message here." className="min-h-28 bg-gray-300"/>
             <Label className="text-xs ml-3 font-light text-muted-foreground">Max. 100 characters</Label>
             <Button className="px-3 text-center rounded-2xl text-sm py-1 border border-[#F0B000] h-7" variant="outline">Continue to payment $000</Button>
           </div>
           <div className="min-h-52"></div>
         </div>
-        <div className="w-[50%] h-auto previewimg flex flex-col justify-center items-center">
-          <div className="w-full h-full p-20 flex flex-col">
-            <Label className="text-white text-[1vw] mt-2">Recipient's Gift Voucher Preview (Email)</Label>
-            <div className="inneremail self-center"></div>
-            <Label className="text-white text-[1vw]">Dear (recipient name),</Label>
-            <Label className="text-white text-[1vw] mt-6">You have received a 1-hour driving lesson gift voucher
+        <div className="w-full md:w-2/3 lg:w-1/2 h-max relative inline-block">
+          <img src="/previewemail.png" alt="preview-email" className="w-full h-auto block" />
+          <div className="w-full h-full px-20 py-16 sm:px-24 sm:py-12 md:px-20 lg:px-24 xl:px-32 flex flex-col justify-center absolute top-0 left-0 text-xs md:text-[0.25rem] lg:text-lg">
+            <Label className="text-white mt-2">Recipient's Gift Voucher Preview (Email)</Label>
+            <div className="bg-innerEmail bg-contain bg-no-repeat bg-center w-8/12 h-2/4 self-center"></div>
+            <Label className="text-white">Dear (recipient name),</Label>
+            <Label className="text-white mt-6">You have received a 1-hour driving lesson gift voucher
               from (sender's name)</Label>
           </div>
         </div>
