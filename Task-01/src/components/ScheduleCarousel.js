0271e86795edd43198c23ae14290d28daf0a2ee9
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-11-20 23:50:59 +0500

    few fixes in main page

--- Task-01/src/components/ScheduleCarousel.js
+++ Task-01/src/components/ScheduleCarousel.js
@@ -5,15 +5,15 @@ export default function ScheduleCarousel() {
     return (
         <div>
             <Carousel className="">
-                <CarouselContent className="">
-                    <CarouselItem>
+                <CarouselContent className="lg:flex lg:space-x-4">
+                    <CarouselItem className="lg:w-1/2">
                         <div className='h-max flex items-center justify-center'>Time
                             <span className="ml-3 buttonDark"><span className="text-green-700">✓ </span>AM</span>
                             <span className="buttonLight">AM</span>
                             <span className="buttonLight">AM</span>
                         </div>
                     </CarouselItem>
-                    <CarouselItem>
+                    <CarouselItem className="lg:w-1/2">
                         <div className='h-max flex items-center justify-center'>Day
                             <span className="ml-3 buttonDark"><span className="text-green-700">✓ </span>Today - Sat</span>
                             <span className="buttonLight">Sun - Wed</span>
